9629d06217b9e681d0e4a1164f9565b4351ba8d4
Neelabh Singh <neelabh041997@gmail.com>
2023-01-02 21:05:33 +0530

    Tasks.js middleware

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks') 
+
+// Middlewares
+app.use(express.json())
+// Routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+const PORT = 3000;
+app.listen(PORT, console.log(`Server is listening on ${PORT}...`))
